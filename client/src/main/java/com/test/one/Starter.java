0ee3726087a16edf4f480b3d4acf91fd7bee5948
svozniuk <sergii@vozniuk.com>
2015-09-02 17:20:51 +0200

    Preparing project for another bug report

---
+++ client/src/main/java/com/test/one/Starter.java
@@ -0,0 +1,7 @@
+package com.test.one;
+
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("Hello from client starter");
+    }
+}
---
+++ server/src/main/java/com.test.two/Starter.java
@@ -0,0 +1,7 @@
+package com.test.two;
+
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("Hello from Server");
+    }
+}
